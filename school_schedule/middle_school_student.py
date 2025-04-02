e8d9489063625c5231d7bd55819225a01a9775e3
Stella Sun <stellasun0332@gmail.com>
2025-04-02 13:37:45 -0500

    created middle school student class

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,19 @@
 from .student import Student
 
+
 # add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, gets_transportation=False):
+        super().__init__(name, grade, classes)
+        self.gets_transportation = gets_transportation
+
+    def summary(self):
+        student_summary = super().summary()
+        transportation_summary = self.transportation_message()
+
+        return student_summary + "\n" + transportation_summary
+
+    def transportation_message(self):
+        has_message = "has" if self.gets_transportation else "doesn't have"
+
+        return f"{self.name} {has_message} transportation"
